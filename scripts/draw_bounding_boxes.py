c99de99fb64cdf35d6b43c014fe37290072171ee
MaslinuPoimal <mykyta.denysov@gmail.com>
2018-05-23 22:27:14 +0200

    Add argparse to the scripts

--- scripts/draw_bounding_boxes.py
+++ scripts/draw_bounding_boxes.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import sys
 import cv2
+import argparse, os
+
+parser = argparse.ArgumentParser(description="Visualize the output of sign_label_plugin")
+parser.add_argument("csv_file", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
+args = parser.parse_args()
 
 # change this to the output folder specified in the launch file
-source_folder = '/hdd/ground_truth/'
-df = pd.read_csv(source_folder + 'ground_truth.csv')
+source_folder = os.path.dirname(args.csv_file.name)
+df = pd.read_csv(args.csv_file.name)
 for index, row in df.iterrows():
-    img = cv2.imread(source_folder + row['Filename'], cv2.IMREAD_UNCHANGED)
+    img = cv2.imread(source_folder + '/' + row['Filename'], cv2.IMREAD_UNCHANGED)
     cv2.rectangle(img, (int(row['Roi.X1']), int(row['Roi.Y1'])), (int(row['Roi.X2']), int(row['Roi.Y2'])), (0, 255, 0))
     cv2.imshow('image',img)
     cv2.waitKey(10000)
